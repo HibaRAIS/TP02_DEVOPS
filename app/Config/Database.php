16de4fab6250041dc16c091694ec6b056ce8b80a
HibaRAIS <hibarais03@gmail.com>
2025-04-26 05:53:59 +0100

    Workflow Test27 Facture

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -20,7 +20,7 @@ class Database extends Config
             'DSN'          => '',
             'hostname'     => env('DB_HOST', 'localhost'),
             'username'     => env('DB_USER', 'root'),
-            'password'     => env('DB_PASSWORD', 'root'),
+            'password'     => env('DB_PASSWORDD', 'root'),
             'database'     => env('DB_NAME', 'TP02_DEVOPS'),
             'DBDriver'     => 'MySQLi',
             'DBPrefix'     => '',
