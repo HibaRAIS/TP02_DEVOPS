e53665f81fdf639fd7fd1128d9c18f38ca2004c1
HibaRAIS <hibarais03@gmail.com>
2025-04-26 03:18:07 +0100

    Workflow Test20 Facture

--- app/Models/FactureModel.php
+++ app/Models/FactureModel.php
@@ -5,7 +5,7 @@ use CodeIgniter\Model;
 
 class FactureModel extends Model
 {
-    protected $table = 'test_factures';
+    protected $table = 'factures';
     protected $primaryKey = 'id';
     protected $allowedFields = ['numero', 'date', 'client', 'montant_total'];
 }
